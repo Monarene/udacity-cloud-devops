be2cb0ee5e11c5647bb7b16a7676e1abb9976ee8
Michael Mekuleyi <mekuleyimichael@gmail.com>
2022-09-16 05:03:02 +0100

    code that works

--- deployments/Lesson-3-Containerization/docker.sh
+++ deployments/Lesson-3-Containerization/docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image
-docker build --tag=demolocal .
+docker build -t flaskapp .
 
 # Step 2: 
 # List docker images
@@ -12,4 +12,4 @@ docker image ls
 
 # Step 3: 
 # Run flask app
-docker run -it demolocal bash
+docker run -p 8000:5001 flaskapp
